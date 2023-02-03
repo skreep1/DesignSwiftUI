52809d0c3d49c6339e76f559ae3fb72284b3d07c
Maksim Skrypka <skrypka.m.l@gmail.com>
2023-02-03 19:41:10 +0200

    Initial Commit

---
+++ DesignSwiftUI/DesignSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignSwiftUIApp.swift
+//  DesignSwiftUI
+//
+//  Created by Maksim Skrypka on 03.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct DesignSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
